cdbb7ae3951bea27ed8627a9f9354d932f48d0e9
AhmedHanafy725 <ahmed.hanafy725@gmail.com>
2018-10-24 13:37:34 +0200

    edit

--- format.py
+++ format.py
@@ -5,7 +5,7 @@ with open('output.log','r') as f:
 # get status and name of test 
 with open('test.log','a') as ff:
     for line in lines:       
-        ff.write(line[line.find('status') + len('status": "') : line.rfind('}') - 1])
+        ff.write(line[line.find('status') + len('status": "') : 'status') + len('status": "') + 4])
         ff.write(' ----> ')
         ff.write(line[line.find('name": "minio-py:') + len('name": "minio-py:') : line.find(',', line.find('name": "minio-py:')) - 1])
         ff.write('\n')
