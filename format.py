69c046093af9cde190037a08f8789adc96c59256
AhmedHanafy725 <ahmed.hanafy725@gmail.com>
2018-10-24 12:58:35 +0200

    format edit

--- format.py
+++ format.py
@@ -7,6 +7,6 @@ with open('test.log','a') as ff:
     for line in lines:       
         ff.write(line[line.find('status') + len('status": "') : line.rfind('}') - 1])
         ff.write(' ----> ')
-        ff.write(line[line.find('minio-py') + len('minio-py:') : line.find(',') - 1])
+        ff.write(line[line.find('name": "minio-py:') + len('name": "minio-py:') : line.find(',', line.find('name": "minio-py:')) - 1])
         ff.write('\n')
     
