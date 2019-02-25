7e5c0970b1d96aa685db81ddedce237827b4ddc6
ahmedhanafy725 <hanafya@codescalers.com>
2019-02-25 15:14:02 +0200

    add night build crontab

--- jsx_machine/3_write_files.py
+++ jsx_machine/3_write_files.py
@@ -1,28 +1,28 @@
 from Jumpscale import j
 from subprocess import run, PIPE
-import os 
+import os
 
 
 # telegram bot
 cl = j.clients.telegram_bot.get("test", bot_token_=os.environ.get('telegram_bot_token'))
 cl.save()
 
-# docker 
+# docker
 run('systemctl stop docker', shell=True)
-with open('/lib/systemd/system/docker.service', 'r') as f: 
+with open('/lib/systemd/system/docker.service', 'r') as f:
     lines = f.readlines()
 with open('/lib/systemd/system/docker.service', 'w+') as f:
-    for line in lines: 
-        if line.startswith('ExecStart=/usr/bin/docker'): 
-            line = "ExecStart=/usr/bin/docker -g /mnt/data -H fd://\n" 
+    for line in lines:
+        if line.startswith('ExecStart=/usr/bin/docker'):
+            line = "ExecStart=/usr/bin/dockerd -g /mnt/data -H fd://\n"
         f.write(line)
 
 with open('/etc/default/docker', 'r') as f:
     lines = f.readlines()
-with open('/etc/default/docker', 'w+') as f: 
-    for line in lines: 
-        if line.startswith('#DOCKER_OPTS='): 
-            line = 'DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4 -g /mnt/data"' 
+with open('/etc/default/docker', 'w+') as f:
+    for line in lines:
+        if line.startswith('#DOCKER_OPTS='):
+            line = 'DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4 -g /mnt/data"'
         f.write(line)
 
 run('systemctl daemon-reload', shell=True)
@@ -40,8 +40,25 @@ export commit=$2
 python3.6 /sandbox/code/github/threefoldtech/jumpscaleX/scripts/autotest.py
 """.format(os.environ.get('telegram_chat_id'), os.environ.get('NACL_SECRET'), os.environ.get('public_ip'), os.environ.get('server_port'))
 
-with open('/home/test.sh', 'w+') as f:
+with open('/mnt/data/scripts/test.sh', 'w+') as f:
     f.write(test_script)
+
+# image build
+build = """set -e
+source /sandbox/env.sh
+cd /mnt/data/result
+export chat_id=@hamadarealtest
+export ServerIp='http://188.165.233.148:7070'
+python3.6 /sandbox/code/github/threefoldtech/jumpscaleX/scripts/build_image.py
+"""
+
+with open('/mnt/data/scripts/bulid.sh', 'w+') as f:
+    f.write(test_script)
+
+# night build for jumpscale image
+crontab = "0 22 * * 0-5 bash /mnt/data/scripts/build.sh"
+with open('/var/spool/cron/crontabs/root', 'w+') as f:
+    f.write(crontab)
 
 # caddy server
 caddy = """:5050 {
