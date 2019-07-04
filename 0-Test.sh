29ee91d4b2bd9e2e65c77b472b0a815d747df370
AhmedHanafy725 <41957921+AhmedHanafy725@users.noreply.github.com>
2019-07-04 09:04:39 +0200

    Update 0-Test.sh

--- 0-Test.sh
+++ 0-Test.sh
@@ -1,2 +1,2 @@
-cd /root/test; pytest -v testcase.py --junitxml=/test.xml -o junit_suite_name=mytest
 cd /root/test; nosetests-3.4 -v testcase.py --with-xunit --xunit-file=/test.xml --xunit-testsuite-name=mytest
+cd /root/test; pytest -v testcase.py --junitxml=/test.xml -o junit_suite_name=mytest
